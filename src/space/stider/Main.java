0997455e5b19c5028e0cba129bad92033da3dcfd
stiderspace <stiderspace@gmail.com>
2015-08-16 19:12:28 +0200

    first commit

---
+++ src/space/stider/Main.java
@@ -0,0 +1,22 @@
+package space.stider;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class Main {
+
+	public static void createDisplay() {
+
+	}
+
+	public static void gameLoop() {
+
+	}
+
+	public static void quit() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
